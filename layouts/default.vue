cf8d92d09e7256261c3195f8d20e0f9149f27299
L <255106739@qq.com>
2025-11-27 11:31:28 +0800

    layouts/default.vue 文件内容出现了重复粘贴的情况，导致语法错误

--- layouts/default.vue
+++ layouts/default.vue
@@ -19,27 +19,6 @@
 // 使用主题组合式函数
 const { updateTheme } = useTheme()
 
-<template>
-  <div class="min-h-screen flex flex-col">
-    <!-- 头部导航 -->
-    <Header />
-    
-    <!-- 主要内容区域 -->
-    <main class="flex-1 pt-24">
-      <slot />
-    </main>
-    
-    <!-- 页脚 -->
-    <Footer />
-  </div>
-</template>
-
-<script setup>
-// 导入组件（Nuxt 3 会自动导入 components 目录下的组件）
-
-// 使用主题组合式函数
-const { updateTheme } = useTheme()
-
 // 记录访问量
 onMounted(async () => {
   try {
